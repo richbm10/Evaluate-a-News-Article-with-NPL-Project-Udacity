6b2a84a7addcab8ba3be207e5d12dba841574e51
richbm10 <ricardobonilla99@hotmail.com>
2020-05-01 13:36:58 -0600

    select

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -2,8 +2,8 @@ import { Services } from './modules/services';
 
 import './styles/pages/index.scss';
 
-import lightBulbH from '../assets/images/light-bulb-h.jpg';
-// import dropDown from '../assets/icons/arrow_drop_down_circle-24px.svg';
+// import lightBulbH from '../assets/images/light-bulb-h.jpg';
+import dropDown from '../assets/icons/arrow_drop_down_circle-24px.svg';
 import fact from '../assets/icons/fact.svg';
 import negative from '../assets/icons/negative.svg';
 import neutral from '../assets/icons/neutral.svg';
@@ -11,10 +11,12 @@ import opinion from '../assets/icons/opinion.svg';
 import positive from '../assets/icons/positive.svg';
 import question from '../assets/icons/question.svg';
 
-const heroImg = document.querySelector('#hero-img');
-heroImg.src = lightBulbH;
+// const heroImg = document.querySelector('#hero-img');
+// heroImg.src = lightBulbH;
 
-//const dropDownIcon = dropDown;
+const dropDownIcon = document.querySelector('#drop-down-icon');
+dropDownIcon.data = dropDown;
+
 const icons = {
     default: question,
     polarityIcons: [question, positive, neutral, negative],
