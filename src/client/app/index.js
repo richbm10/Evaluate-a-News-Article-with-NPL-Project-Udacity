d82544f3f72277c6f85c028cf3243d2488161098
richbm10 <ricardobonilla99@hotmail.com>
2020-04-30 20:41:16 -0600

    services-module

---
+++ src/client/app/index.js
@@ -0,0 +1,10 @@
+import { Services } from './modules/services';
+
+import './styles/pages/index.scss';
+
+import lightBulbH from '../assets/images/light-bulb-h.jpg';
+
+const heroImg = document.querySelector('#hero-img');
+heroImg.src = lightBulbH;
+
+export { Services };
