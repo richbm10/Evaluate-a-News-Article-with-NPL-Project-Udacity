b45573aa60e4ababd9738d33158e80f564b8d6f6
richbm10 <ricardobonilla99@hotmail.com>
2020-05-05 20:33:25 -0600

    before adding it for test promises

---
+++ tests/client/form.test.js
@@ -0,0 +1,65 @@
+import "babel-polyfill";
+const { Services } = require('../../src/client/app/modules/services');
+
+const services = Services.getInstance();
+services.set('http://localhost:8000');
+
+function validateUrl(value) { //taken from https://stackoverflow.com/questions/8667070/javascript-regular-expression-to-validate-url
+    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
+
+function submit(query, inputToEval, expectedOutput) {
+    services.postRequestLocalServer(query, { 'content': inputToEval }).then((response) => {
+        try {
+            services.handleResponse(response, () => {
+                expect(services.serviceData).toMatchObject(expectedOutput);
+            });
+        } catch (error) {
+            expect(error).toMatchObject(expectedOutput);
+        }
+    }).catch(() => {
+        expect('503 Server Error Connection').toMatchObject(expectedOutput);
+    });
+}
+
+function formButtonClick(form, expectedOutput) {
+    const activeFormInput = form.activeFormInput;
+    let query;
+    let inputToEval;
+    if (activeFormInput === 'url') {
+        inputToEval = form.urlToEval;
+        query = services.queryNlpUrl;
+        if (validateUrl(inputToEval)) {
+            submit(query, inputToEval, expectedOutput);
+        } else {
+            expect('Error Not Valid URL Structure').toMatchObject(expectedOutput);
+        }
+    } else if (activeFormInput === 'text') {
+        inputToEval = form.textToEval;
+        query = services.queryNlpText;
+        submit(query, inputToEval, expectedOutput);
+    }
+}
+
+
+describe("submit form", () => {
+    test("it should respond a success message with polarity and subjectivity for each of them", () => {
+        const input = [
+            // { activeFormInput: 'url', urlToEval: 'https://codequickie.com/what-is-python-and-what-can-you-do-with-it/' },
+            { activeFormInput: 'text', textToEval: 'I had a very good time in the restaurant, the service was really good.' }
+        ];
+
+        const output = {
+            cod: 200,
+            message: 'Success',
+            content: {
+                polarity: expect.stringMatching(/positive|negative|neutral /),
+                subjectivity: expect.stringMatching(/subjective|objective/)
+            }
+        };
+
+        for (let formPrototype of input) {
+            formButtonClick(formPrototype, output);
+        }
+    });
+});
---
+++ tests/server/index-test.js
@@ -0,0 +1,81 @@
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+/* Initializing the main project folder */
+app.use(express.static('tests/client'));
+
+const port = 8000;
+
+function logActiveServer() {
+    console.log('Server running..');
+    console.log(`localhost: ${port}`);
+}
+
+app.listen(port, () => {
+    logActiveServer();
+});
+
+// ServerServices for requesting external services
+const dotenv = require('dotenv');
+dotenv.config();
+const { ServerServices } = require('../../src/server/services');
+const externalServices = ServerServices.getInstance();
+externalServices.set(process.env.AYLIEN_API_ID, process.env.AYLIEN_API_KEY);
+
+function successMessage(pContent) {
+    return {
+        cod: 200,
+        message: 'Success',
+        content: pContent
+    };
+}
+
+function errorMessage(pContent) {
+    return {
+        cod: 500,
+        message: 'Internal Server Error',
+        content: pContent
+    };
+}
+
+function requestAylienApi(endpoint, type, request, response) {
+    const apiParam = externalServices.setSentimentAylienApiParam(type, request.body);
+    let message;
+
+    externalServices.getSentimentAylienApi(apiParam, (externalResponse) => {
+        message = successMessage(externalResponse);
+        response.send(message);
+        logActivatedService(`HTTP POST Service: ${endpoint}`, request.body, message);
+    }, (error) => {
+        message = errorMessage(error);
+        response.send(message);
+        logActivatedService(`HTTP POST Service: ${endpoint}`, request.body, message);
+    });
+}
+
+app.post('/nlp/evaltext', (request, response) => {
+    requestAylienApi('/nlp/evaltext', 'text', request, response);
+});
+
+app.post('/nlp/evalurl', (request, response) => {
+    requestAylienApi('/nlp/evalurl', 'url', request, response);
+});
+
+function logActivatedService(service, requestLog, responseLog) {
+    console.log(service);
+    console.log('\nBody Request:\n', requestLog);
+    console.log('\nBody Response:\n', responseLog);
+    logActiveServer();
+}
